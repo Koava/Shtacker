738f3cbe3f6ce1393d1626e134879e648efc1fd7
ptmclean <ptmclean@gmail.com>
2013-10-25 13:17:37 +1100

    The basic game is in place. Refactoring and polish needed.

--- jQueryShtacker/jQueryShtacker/TypeScriptShtacker/public/game.ts
+++ jQueryShtacker/jQueryShtacker/TypeScriptShtacker/public/game.ts
@@ -14,6 +14,9 @@ class Game {
     private stage: createjs.Stage;
     private stageBg: createjs.Bitmap;
     private menuContainer: createjs.Container;
+    private youLoseContainer: createjs.Container;
+    private youWinContainer: createjs.Container;
+
 
     private titleFont: string = "bold 56px Comic Sans MS";
     private menuFont: string = "bold 32px Comic Sans MS";
@@ -74,20 +77,18 @@ class Game {
 
     private circle141: createjs.Shape;
 
-
+    private gameOver: boolean;
     private direction: string;
     private clicked: boolean;
+    private previousRowRange: Array<number>;
     constructor() {
-        
-    }
-
-    initiate() {
         this.canvas = <HTMLCanvasElement>document.getElementById('gameCanvas');
         this.stage = new createjs.Stage(this.canvas);
+        this.initiate();
+    }
 
+    initiate() {   
         createjs.Ticker.setFPS(1000);
-        // in order for the stage to continue to redraw when the Ticker is paused we need to add it with
-        // the second ("pauseable") param set to false.
         createjs.Ticker.addListener(this.stage, false);
 
         //Can't get this working, what's up with this?
@@ -97,7 +98,6 @@ class Game {
         gradient.graphics.beginLinearGradientFill(["#000", "#141654"], [0, 1], 0, 0, this.stageWidth, this.stageHeight).drawRect(0, 0, this.stageWidth, this.stageHeight);
         gradient.graphics.endFill();
         this.stage.addChild(gradient);
-        this.stage.update();
 
         this.menuContainer = new createjs.Container();
 
@@ -121,9 +121,149 @@ class Game {
         this.menuContainer.x = 600;           
         this.stage.addChild(this.menuContainer);
         this.showMenu();
+
+        this.youLoseContainer = new createjs.Container();
+        var title: createjs.Text = new createjs.Text();
+        title.text = "You Lose!";
+        title.color = this.fontColor;
+        title.font = this.titleFont;
+        title.x = 65;
+        title.y = 100;
+        this.youLoseContainer.addChild(title);
+        this.youLoseContainer.x = 600;
+        this.stage.addChild(this.youLoseContainer);
+
+        this.youWinContainer = new createjs.Container();
+        var title: createjs.Text = new createjs.Text();
+        title.text = "You Win!";
+        title.color = this.fontColor;
+        title.font = this.titleFont;
+        title.x = 65;
+        title.y = 100;
+        this.youWinContainer.addChild(title);
+        this.youWinContainer.x = 600;
+        this.stage.addChild(this.youWinContainer);
+
     }
 
-    startPlay(): void {
+    private startPlay(): void {
+        this.reset();
+        this.stage.onPress = () => { this.clicked = true; };
+        createjs.Ticker.addListener(this);
+    }
+
+    private registerClick() {
+        this.clicked = true;
+    }
+
+    private tick() {
+        if (this.currentRow < 15) {
+            if (!this.clicked) {
+                if (this.direction == "back") {
+                    if (this.rows[this.currentRow][0].x > 19) {
+                        for (var i: number = 0; i < this.rows[this.currentRow].length; i++) {
+                            this.rows[this.currentRow][i].x -= this.speed;
+                        }
+                    }
+                    else {//Change Direction
+                        for (var i: number = 0; i < this.rows[this.currentRow].length; i++) {
+                            this.rows[this.currentRow][i].x += this.speed;
+                        }
+                        this.direction = "foward";
+                    }
+                }
+                else { //Foward
+                    if (this.rows[this.currentRow][this.rows[this.currentRow].length - 1].x < 381) {
+                        for (var i: number = 0; i < this.rows[this.currentRow].length; i++) {
+                            this.rows[this.currentRow][i].x += this.speed;
+                        }
+                    }
+                    else {//Change Direction
+                        for (var i: number = 0; i < this.rows[this.currentRow].length; i++) {
+                            this.rows[this.currentRow][i].x -= this.speed;
+                        }
+                        this.direction = "back";
+                    }
+                }
+            }
+            else { //Clicked
+                this.clicked = false;
+                this.dropCircles();
+                if (this.rows[this.currentRow].length == 0) {
+                    //You lose
+                    this.youLose();
+                }
+                else {
+                    this.previousRowRange = [this.rows[this.currentRow][0].x, this.rows[this.currentRow][(this.rows[this.currentRow].length - 1)].x];
+                    this.currentRow++;
+                }
+            }
+
+            if (this.currentRow > 3) {//Speed up
+                this.speed = this.currentRow / 3;
+            }
+        }
+        else {
+            this.youWin();
+        }
+
+    }
+
+    dropCircles() {
+        var newCurrent = [];
+        for (var i: number = 0; i < this.rows[this.currentRow].length; i++) {
+            var xPos = this.rows[this.currentRow][i].x;
+            if (xPos < this.previousRowRange[0] - 20) {
+                createjs.Tween.get(this.rows[this.currentRow][i]).to({ y: 900 }, 1200);
+            }
+            else if (xPos > this.previousRowRange[1] + 20) {
+                createjs.Tween.get(this.rows[this.currentRow][i]).to({ y: 900 }, 1200);
+            }
+            else {
+                newCurrent[newCurrent.length] = this.rows[this.currentRow][i]
+            }
+        }
+        this.rows[this.currentRow] = newCurrent;
+    }
+
+    removeMenu(): void {
+        createjs.Tween.get(this.menuContainer).to({ x: 500 }, 500);
+    }
+
+    showMenu(): void {
+        createjs.Tween.get(this.menuContainer).to({ x: 0 }, 500);
+    }
+
+
+    getCircle(xCentre: number, yCentre: number): createjs.Shape {
+        var circle = new createjs.Shape();
+        circle.graphics.setStrokeStyle(6);
+        circle.graphics.beginLinearGradientStroke(["#FFCC00", "#FFCC66"], [0, 1], 0, 0, 0, 10).drawCircle(0, 0, this.circleRadius);
+        circle.alpha = 0.8;
+        circle.x = xCentre;
+        circle.y = yCentre;
+        return circle;
+    }
+
+    youLose() {
+        createjs.Ticker.removeListener(this);
+        this.stage.onPress = null;
+        createjs.Tween.get(this.youLoseContainer)
+            .to({ x: 0 }, 500)
+            .wait(3000)
+            .to({ x: 500 }, 500)
+            .call(this.initiate, null, this);
+    }
+    youWin() {
+        createjs.Ticker.removeListener(this);
+        this.stage.onPress = null;
+        createjs.Tween.get(this.youWinContainer)
+            .to({ x: 0 }, 500)
+            .wait(3000)
+            .to({ x: 500 }, 500)
+            .call(this.initiate, null, this);
+    }
+    reset() {
         this.removeMenu();
         this.circle1 = this.getCircle(440, 581);
         this.circle2 = this.getCircle(478, 581);
@@ -222,95 +362,18 @@ class Game {
         this.rows.push([this.circle131]);
         this.rows.push([this.circle141]);
 
+        this.previousRowRange = [0, this.stageWidth];
 
         this.currentRow = 0;
 
         this.direction = "back";
         this.speed = 1;
         this.clicked = false;
-        
-        this.stage.onPress = () => { this.clicked = true; };
-        createjs.Ticker.addListener(this)
-        //createjs.Tween.get(circle1).to({ x: 19 }, this.speed).to({ x: 342 }, this.speed);
-        //createjs.Tween.get(circle2).to({ x: 58 }, this.speed).to({ x: 381 }, this.speed);
-
-        //circle1.x = 342;
-        //circle2.x = 381;
-
-        //createjs.Tween.get(circle1, { loop: true }).to({ x: 19 }, this.speed).to({ x: 342 }, this.speed).on;
-        //createjs.Tween.get(circle2, { loop: true }).to({ x: 58 }, this.speed).to({ x: 381 }, this.speed);
-
-
         this.stage.update();
     }
-
-    private registerClick() {
-        this.clicked = true;
-    }
-
-    private tick() {
-        if (this.currentRow < 15) {
-            if (!this.clicked) {
-                if (this.direction == "back") {
-                    if (this.rows[this.currentRow][0].x > 19) {
-                        for (var i: number = 0; i < this.rows[this.currentRow].length; i++) {
-                            this.rows[this.currentRow][i].x -= this.speed;
-                        }
-                    }
-                    else {
-                        for (var i: number = 0; i < this.rows[this.currentRow].length; i++) {
-                            this.rows[this.currentRow][i].x += this.speed;
-                        }
-                        this.direction = "foward";
-                    }
-                }
-                else {
-                    if (this.rows[this.currentRow][this.rows[this.currentRow].length - 1].x < 381) {
-                        for (var i: number = 0; i < this.rows[this.currentRow].length; i++) {
-                            this.rows[this.currentRow][i].x += this.speed;
-                        }
-                    }
-                    else {
-                        for (var i: number = 0; i < this.rows[this.currentRow].length; i++) {
-                            this.rows[this.currentRow][i].x -= this.speed;
-                        }
-                        this.direction = "back";
-                    }
-                }
-            }
-            else {
-                this.clicked = false;
-                this.currentRow++;
-            }
-            if (this.currentRow > 3) {
-                this.speed = this.currentRow / 3;
-            }
-        }
-    }
-
-    removeMenu(): void {
-        createjs.Tween.get(this.menuContainer).to({ x: 500 }, 500);
-    }
-
-    showMenu(): void {
-        createjs.Tween.get(this.menuContainer).to({ x: 0 }, 500);
-    }
-
-
-    getCircle(xCentre: number, yCentre: number): createjs.Shape {
-        var circle = new createjs.Shape();
-        circle.graphics.setStrokeStyle(6);
-        circle.graphics.beginLinearGradientStroke(["#FFCC00", "#FFCC66"], [0, 1], 0, 0, 0, 10).drawCircle(0, 0, this.circleRadius);
-        circle.alpha = 0.8;
-        circle.x = xCentre;
-        circle.y = yCentre;
-        return circle;
-    }
-
-
 }
+
 
 window.onload = () => {
     var game = new this.Game();
-    game.initiate();
 };
